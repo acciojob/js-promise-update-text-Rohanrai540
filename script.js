a3005939600d9faa536afe0a3e70fe9002e49307
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-03-16 23:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function getMessage() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+getMessage().then(function(result) {
+  document.getElementById("output").textContent = result;
+});
